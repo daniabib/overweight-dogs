935acb7d33356a631ef348f7f0333c19de1774e4
Daniel Abib <danielsabib@gmail.com>
2022-03-26 13:16:10 -0300

    Image processing working locally

--- frontend/ui.py
+++ frontend/ui.py
@@ -2,18 +2,34 @@ import string
 import requests
 from PIL import Image
 from PIL.ExifTags import TAGS
+
 import streamlit as st
 
+import io
 import boto3
 
 BACKEND = "http://backend:8080/prediction"
 SUPPORTED_TYPES = ['png', 'jpg', 'jpeg', 'bmp', 'gif', 'tga', 'tiff']
 
 
+def format_bytes(size):
+    # 2**10 = 1024
+    power = 2**10
+    n = 0
+    power_labels = {0 : '', 1: 'kilo', 2: 'mega', 3: 'giga', 4: 'tera'}
+    while size > power:
+        size /= power
+        n += 1
+    return size, power_labels[n]+'bytes'
+
+
 def process_image(image) -> Image:
     """Transform a BytesIO object into a PIL Image.
        Also handle image orientation."""
-    image = Image.open(image)
+    # image = Image.open('nico.jpg')
+    image_byte_size = len(image)
+    image = Image.open(io.BytesIO(image))
+
     orientation = None
     for key, value in image.getexif().items():
         if TAGS.get(key) == "Orientation":
@@ -26,7 +42,29 @@ def process_image(image) -> Image:
     if orientation == 8:
         image = image.transpose(Image.ROTATE_90)
 
-    return image
+
+    # TODO: Resize image if its over rekognition size limit
+    if image_byte_size >= 5042880:
+        width, height = image.size
+
+        print("Image size in bytes before resize: ", format_bytes(image_byte_size))
+        image = image.resize((int(width*.4), int(height*.4)), resample=Image.ANTIALIAS)
+        buffer = io.BytesIO()
+        image.save(buffer, format="JPEG", quality=40)
+        # image.close()
+        image_bytes = buffer.getvalue()
+
+        # image = Image.open(io.BytesIO(image_bytes))
+        # image_bytes2 = image.tobytes()
+        print("Image size in bytes(2) AFTER resize: ", format_bytes(len(image_bytes)))
+        return image_bytes
+
+    buffer = io.BytesIO()
+    image.save(buffer, format="JPEG", quality=40)
+    # image.close()
+    image_bytes = buffer.getvalue()
+    
+    return image_bytes
 
 
 def detect_labels_rekognition(image):
@@ -88,24 +126,39 @@ st.header("Is my dog overweighted?")
 
 st.write("This is simple application that tells by the image of your dog if it is overweighted.")
 
-input_image = st.file_uploader(
-    "insert image", type=SUPPORTED_TYPES)
+input_image = st.file_uploader("Insert image", type=SUPPORTED_TYPES)
 
 if st.button("Upload your dog image"):
     if input_image:
         bytes_image = input_image.getvalue()
-        output_image = process_image(input_image)
+        output_image = process_image(bytes_image)
+        # output_image = process_image(input_image.getvalue())
+        # print("Type bytes_image: ", type(bytes_image))
+        print("Byte size output_im1: ", format_bytes(len(output_image)))
+
+        # print(output_image)
         st.image(output_image, caption='Uploaded Image.',
                  use_column_width=True)
         st.write("")
-        # classification = classify(bytes_image, BACKEND)
-        # st.header(f"Your dog seens {classification}.")
-        is_dog, detected_label, confidence  = detect_labels_rekognition(bytes_image)
+
+        # output_image = output_image.tobytes()
+        
+        print("Type output_image: ", type(output_image))
+        print("Byte size output_im2: ", format_bytes(len(output_image)))
+
+        is_dog, detected_label, confidence  = detect_labels_rekognition(output_image)
+        # is_dog, detected_label, confidence  = detect_labels_rekognition(bytes_image)
+        # detected_label = "TEST: REKOGNITION"
+        # is_dog = True
         if is_dog:
-            classification = classify(bytes_image, BACKEND)
+            # classification = classify(output_image, BACKEND)
+            classification = "TEST"
             st.header(f"Your dog seens {classification}.")
+        
         else:
-            st.header(f"Are you sure it's a gog? If seens more like a {detected_label}.")
+            st.header(f"Are you sure it's a dog? If seens more like a {detected_label}.")
+        
+
     else:
         st.write("Insert an image!")
 
